4fc31ddc3b2e042e8bd3562e668c7da86d981ea1
Maria Soulountsi <mariasoul01@gmail.com>
2019-07-15 13:22:07 +0200

    add put method in FreshchatClient

--- freshchat/client/client.py
+++ freshchat/client/client.py
@@ -101,6 +101,25 @@ class FreshChatClient(LoggedObject):
             method="POST", endpoint=endpoint, params=params, json=json, headers=headers
         )
 
+    async def put(
+        self,
+        endpoint: str,
+        params: Optional[Dict[AnyStr, Any]] = None,
+        json: Optional[Dict[AnyStr, AnyStr]] = None,
+        headers: Optional[Dict[str, Any]] = None,
+    ) -> FreshChatResponse:
+        """
+        Method used for the put requests
+
+        :param endpoint: Resource endpoint
+        :param params: request parameters
+        :param json: request json body
+        :param headers: Additional request headers
+        """
+        return await self.request(
+            method="PUT", endpoint=endpoint, params=params, json=json, headers=headers
+        )
+
     def __repr__(self):
         return (
             f"{self.__class__.__name__}<{hex(id(self))}> (config={repr(self.config)})"
