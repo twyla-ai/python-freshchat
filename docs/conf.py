85e34d14ed4d1b9ee4042bb26e82b5e5afe6ae0f
Maria Soulountsi <mariasoul01@gmail.com>
2019-10-15 13:45:58 +0200

    Mock external dependencies which create issues with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = "Maria Soulountsi, Arun Neelicattu"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "readthedocs_ext.readthedocs"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -39,6 +39,9 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+
+# This value contains a list of modules to be mocked up.
+autodoc_mock_imports = ["aiohttp", "cafeteria", "Crypto"]
 
 # -- Options for HTML output -------------------------------------------------
 
